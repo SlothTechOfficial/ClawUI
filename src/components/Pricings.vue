ab4f466db6ed985647a9a9c2b0e105686965b0e7
monster <josemonzter50@gmail.com>
2019-10-12 13:40:30 -0500

    First commit

---
+++ src/components/Pricings.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="communities">
+    <div class="container">
+      <h1 class="title-section">Speakers</h1>
+      <ul class="communities-list">
+        <li class="communities-item">
+          <article class="community">
+            <div class="community__image">
+              <img src="https://source.unsplash.com/1600x900/?portrait" alt>
+            </div>
+            <div class="community__content">
+              <div class="community__info">Talent Nights QRoo</div>
+              <div class="community__social">
+                <a href="#" target="_blank">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" target="_blank">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#" target="_blank">
+                  <i class="fas fa-globe-americas"></i>
+                </a>
+              </div>
+            </div>
+          </article>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Speakers"
+};
+</script>
