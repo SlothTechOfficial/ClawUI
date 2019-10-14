6a229aa9b09aed44fb3458f405684d8526636af4
Vijay Krishna <vijaykrishna@Vijays-MacBook-Pro.local>
2019-10-15 00:19:54 +0530

    CSS fix, few unwanted tags removed

--- src/components/NewsLetter.vue
+++ src/components/NewsLetter.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="newsletter">
-        <img src="https://images.pexels.com/photos/1109543/pexels-photo-1109543.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+        <img :src="source">
         <div class="newsletter__subscription">
             <div class="newsletter__title">Stay Updated</div>
             <div class="newsletter__subtitle"> Give a brief reason for people to subscribe to newsletter</div>
@@ -8,7 +8,7 @@
                 <span class="newsletter__container">
                     <input class="newsletter__text" type="email" v-model="email">
                 </span>
-                <button class="newsletter__button" @click="submit()">  Subscribe
+                <button class="button--primary secondary" @click="submit()">  Subscribe
                 </button> 
             </div>   
         </div>
@@ -21,6 +21,7 @@ export default {
     data () {
         return {
             email:'',
+            source:'https://images.pexels.com/photos/1109543/pexels-photo-1109543.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
         }
     },
     methods: {
@@ -30,6 +31,3 @@ export default {
     }
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
