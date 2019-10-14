476092379275054abd88444851b95bf43763796e
Jose Antonio <antonio.gonzalez@bdtravelsolutions.com>
2019-10-14 14:06:04 -0500

    add data for use cases real, use BEM and Center text

--- src/components/Pricings.vue
+++ src/components/Pricings.vue
@@ -3,60 +3,85 @@
     <div class="container">
       <h1 class="title-section">Prices</h1>
       <div class="pricings-list">
-          <div class="table-price">
-              <div class="table-price__head">
-                  <h2 class="table-price__title">Basic</h2>
-                  <p class="table-price__price">Free</p>
-              </div>
-              <div class="table-price__body">
-                  <ul class="table-price__list">
-                      <li class="table-price__items">Lorem ipsum</li>
-                      <li class="table-price__items">Lorem ipsum</li>
-                      <li class="table-price__items">Lorem ipsum</li>
-                  </ul>
-              </div>
-              <div class="table-price__select">
-            
-                  <button class="button-lg--outline-secondary">Select</button>
-                  <p class="table-price__small-title">lorem ipsum</p>
-              </div>
+        <div class="table-price">
+          <div class="table-price__head">
+            <h2 class="table-price__title">Basic</h2>
+            <p class="table-price__price">Free</p>
           </div>
-          <div class="table-price select">
-              <div class="table-price__head">
-                  <h2 class="table-price__title">Premium</h2>
-                  <p class="table-price__price"> $1,299</p>
-              </div>
-              <div class="table-price__body">
-                  <ul class="table-price__list">
-                      <li class="table-price__items">Lorem ipsum</li>
-                      <li class="table-price__items">Lorem ipsum</li>
-                      <li class="table-price__items">Lorem ipsum</li>
-                  </ul>
-              </div>
-              <div class="table-price__select">
-            
-                  <button class="button-lg--secondary">Select</button>
-                  <p class="table-price__small-title">lorem ipsum</p>
-              </div>
+          <div class="table-price__body">
+            <ul class="table-price__list">
+              <li class="table-price__items">
+              
+                Entrance dsds
+              </li>
+              <li class="table-price__items">
+              
+                Coffee Break
+              </li>
+           
+            </ul>
           </div>
-          <div class="table-price ">
-              <div class="table-price__head">
-                  <h2 class="table-price__title">Estandar</h2>
-                  <p class="table-price__price">$899</p>
-              </div>
-              <div class="table-price__body">
-                  <ul class="table-price__list">
-                      <li class="table-price__items">Lorem ipsum</li>
-                      <li class="table-price__items">Lorem ipsum</li>
-                      <li class="table-price__items">Lorem ipsum</li>
-                  </ul>
-              </div>
-              <div class="table-price__select">
-            
-                  <button class="button-lg--outline-secondary">Select</button>
-                  <p class="table-price__small-title">lorem ipsum</p>
-              </div>
+          <div class="table-price__select">
+            <button class="button-lg--outline-secondary">Select</button>
+            <p class="table-price__small-title">lorem ipsum</p>
           </div>
+        </div>
+        <div class="table-price table-price--recomend">
+          <div class="table-price__head">
+            <h2 class="table-price__title">Premium</h2>
+            <p class="table-price__price"><span class="currency"> <span class="currency"> $</span></span>1,299</p>
+          </div>
+          <div class="table-price__body">
+            <ul class="table-price__list">
+              <li class="table-price__items">
+              
+                Entrance dsds
+              </li>
+              <li class="table-price__items">
+              
+                Coffee Break
+              </li>
+              <li class="table-price__items">
+              
+                Certificate
+              </li>
+              <li class="table-price__items">
+              
+                Workshop
+              </li>
+            </ul>
+          </div>
+          <div class="table-price__select">
+            <button class="button-lg--secondary">Select</button>
+            <p class="table-price__small-title">lorem ipsum</p>
+          </div>
+        </div>
+        <div class="table-price">
+          <div class="table-price__head">
+            <h2 class="table-price__title">Estandar</h2>
+            <p class="table-price__price"><span class="currency"> $</span>899</p>
+          </div>
+          <div class="table-price__body">
+            <ul class="table-price__list">
+              <li class="table-price__items">
+              
+                Entrance dsds
+              </li>
+              <li class="table-price__items">
+              
+                Coffee Break
+              </li>
+              <li class="table-price__items">
+              
+                Certificate
+              </li>
+            </ul>
+          </div>
+          <div class="table-price__select">
+            <button class="button-lg--outline-secondary">Select</button>
+            <p class="table-price__small-title">lorem ipsum</p>
+          </div>
+        </div>
       </div>
     </div>
   </section>
